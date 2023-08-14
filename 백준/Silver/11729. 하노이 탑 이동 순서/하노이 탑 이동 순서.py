abb3d47b4236d6a2048c4f149c358e75a37c3485
2BELLBELL <139421092+2BELLBELL@users.noreply.github.com>
2023-08-14 13:49:23 +0900

    [Silver I] Title: 하노이 탑 이동 순서, Time: 876 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.py
@@ -0,0 +1,15 @@
+import sys
+
+N = int(sys.stdin.readline())
+
+def hanoi_tower(n, start, end):
+    if n == 1:
+        print(start, end)
+        return
+
+    hanoi_tower(n - 1, start, 6 - start - end)
+    print(start, end)
+    hanoi_tower(n - 1, 6 - start - end, end)
+
+print(2**N-1)
+hanoi_tower(N, 1, 3)

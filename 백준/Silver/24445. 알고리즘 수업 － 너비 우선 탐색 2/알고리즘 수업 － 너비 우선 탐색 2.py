6ec1266d2f254f218a34d1cab4a0e1f26e0ae89b
2BELLBELL <139421092+2BELLBELL@users.noreply.github.com>
2023-08-18 09:34:05 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 2000 ms, Memory: 59548 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.py
@@ -0,0 +1,39 @@
+import sys
+
+def bfs(graph, v):
+    global cnt
+    que = [v]
+    visited[v] = True
+    answer[v] = cnt
+    # 방문 순서 체크용
+    while que:
+        tmp = que.pop(0)
+        for i in graph[tmp]:
+            if visited[i] == False:
+                que.append(i)
+                visited[i] = True
+                cnt += 1
+                answer[i] = cnt
+
+
+N, M, R = map(int, sys.stdin.readline().split())
+visited = [False] * (N+1)
+graph = [[] for _ in range(N+1)]
+answer = [0] * (N+1)
+cnt = 1
+# 간선 정보 입력 받기
+for i in range(M):
+    start, end = map(int, sys.stdin.readline().split())
+    graph[start].append(end)
+    graph[end].append(start)
+
+# 오름차순으로 방문하기 위한 정렬
+for i in graph:
+    i.sort()
+    i.reverse()
+# bfs 실행
+bfs(graph, R)
+
+# 1번째 정점부터 도착순서 출력
+for i in range(1, N+1):
+    print(answer[i])

2510b140c1ff1168ad5ada2e1d4d93a1d64edff7
2BELLBELL <139421092+2BELLBELL@users.noreply.github.com>
2023-08-26 15:52:48 +0900

    [Bronze I] Title: 색종이, Time: 300 ms, Memory: 38304 KB -BaekjoonHub

--- 백준/Bronze/10163. 색종이/색종이.py
+++ 백준/Bronze/10163. 색종이/색종이.py
@@ -3,17 +3,17 @@ input = sys.stdin.readline
 
 N = int(input())
 arr = [[0] * 1001 for _ in range(1001)]
+idx = [0] * (N + 1)
 for k in range(1, N + 1):
     x1, y1, x2, y2 = map(int, input().split())
     for i in range(x1, x1 + x2):
         for j in range(y1, y1 + y2):
             arr[i][j] = k
 
-for k in range(1, N + 1):
-    cnt = 0
-    for i in range(1001):
-        for j in range(1001):
-            if arr[i][j] == k:
-                cnt += 1
-    print(cnt)
 
+for i in range(1001):
+    for j in range(1001):
+        idx[arr[i][j]] += 1
+
+for i in range(1, N + 1):
+    print(idx[i])

955392d150aee07461c25f4c673ac4b405609d09
2BELLBELL <139421092+2BELLBELL@users.noreply.github.com>
2023-08-26 15:46:39 +0900

    [Bronze I] Title: 색종이, Time: 1240 ms, Memory: 38304 KB -BaekjoonHub

---
+++ 백준/Bronze/10163. 색종이/색종이.py
@@ -0,0 +1,16 @@
+N = int(input())
+arr = [[0] * 1001 for _ in range(1001)]
+for k in range(1, N + 1):
+    x1, y1, x2, y2 = map(int, input().split())
+    for i in range(x1, x1 + x2):
+        for j in range(y1, y1 + y2):
+            arr[i][j] = k
+
+for k in range(1, N + 1):
+    cnt = 0
+    for i in range(1001):
+        for j in range(1001):
+            if arr[i][j] == k:
+                cnt += 1
+    print(cnt)
+

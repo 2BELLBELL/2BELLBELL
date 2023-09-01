c5a83fbceddfe51382469d7dd10958128bf66137
2BELLBELL <139421092+2BELLBELL@users.noreply.github.com>
2023-09-01 17:28:42 +0900

    [Silver I] Title: 신입 사원, Time: 6016 ms, Memory: 72264 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.py
+++ 백준/Silver/1946. 신입 사원/신입 사원.py
@@ -4,20 +4,17 @@ input = sys.stdin.readline
 T = int(input())
 for tc in range(1, T + 1):
     N = int(input())
-    arr = []
-    for _ in range(N):
-        tmp = list(map(int, input().split()))
-        arr.append(tmp)
-    arr.sort()
-    stack = []
+    # 서류 심사 성적별로 정렬
+    arr = list(sorted([list(map(int, input().split())) for _ in range(N)]))
+    # 임시로 설정한 최대 면접 성적
+    top = arr[0][1]
+    # 서류 심사 순위별 순회
     for i in arr:
-        if not stack:
-            stack.append(i[1])
+        # 더 낮은 성적인 사람은 탈락
+        if top < i[1]:
+            N -= 1
+        # 더 높은 성적이면 최대 면접 성적 변경
         else:
-            if stack[-1] < i[1]:
-                N -= 1
-            else:
-                stack.pop()
-                stack.append(i[1])
+            top = i[1]
 
     print(N)

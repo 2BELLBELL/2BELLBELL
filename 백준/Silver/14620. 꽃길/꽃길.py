8c1d1ff2114044f1e66f9fb1e3ccbe7844652cdd
2BELLBELL <139421092+2BELLBELL@users.noreply.github.com>
2023-09-18 11:39:28 +0900

    [Silver II] Title: 꽃길, Time: 188 ms, Memory: 115436 KB -BaekjoonHub

--- 백준/Silver/14620. 꽃길/꽃길.py
+++ 백준/Silver/14620. 꽃길/꽃길.py
@@ -1,49 +1,39 @@
-# 본래 자리를 포함한 4방향에 이미 꽃을 심었다면
-# False를 아니면 True를 리턴
-def check(a, b):
-    for i in range(5):
-        x = a + di[i]
-        y = b + dj[i]
-        if visited[x][y] == 1:
-            return False
-    return True
- 
- 
-def recur(cur):
-    global total, answer
-    
-    # 꽃을 3개 다 심었을 때 최소 코스트 갱신
-    if cur == 3:
-        answer = min(answer, total)
+def back(cost, num, x, y):
+    global answer
+    # 3개의 씨앗을 뿌렸으면 화단비용 체크
+    if num == 3:
+        answer = min(cost, answer)
         return
- 
-    for i in range(1, n - 1):
-        for j in range(1, n - 1):
-            # 5 공간에 이미 심지 않았다면
-            if check(i, j):
-                for k in range(5):
-                    x = i + di[k]
-                    y = j + dj[k]
-                    visited[x][y] = 1
-                    total += g[x][y]
- 
-                recur(cur + 1)
-                
-                # 다음 재귀를 위해 초기화
-                for k in range(5):
-                    x = i + di[k]
-                    y = j + dj[k]
-                    visited[x][y] = 0
-                    total -= g[x][y]
- 
-n = int(input())
-g = [list(map(int, input().split())) for i in range(n)]
- 
-di = [0, -1, 1, 0, 0]
-dj = [0, 0, 0, -1, 1]
- 
-visited = [[0] * n for i in range(n)]
-answer = 99999999
-total = 0
-recur(0)
+
+    for i in range(1, N - 1):
+        for j in range(1, N - 1):
+            # 다른 꽃잎과 닿게 되는 경우는 패스
+            flag = True
+            for k, l in [[-1, 0], [1, 0], [0, -1], [0, 1], [0, 0]]:
+                nx, ny = i + k, j + l
+                if arr[nx][ny] == 999:
+                    flag = False
+            if not flag:
+                continue
+            # 꽃잎과 닿지 않는 구간인 경우 화단 비용을 더하고 씨앗의 위치를 999로 임시 지정
+            cnt = arr[i][j] + arr[i - 1][j] + arr[i + 1][j] + arr[i][j - 1] + arr[i][j + 1]
+            tmp_mid = arr[i][j]
+            tmp_top = arr[i - 1][j]
+            tmp_bot = arr[i + 1][j]
+            tmp_left = arr[i][j - 1]
+            tmp_right = arr[i][j + 1]
+            arr[i][j] = arr[i - 1][j] = arr[i + 1][j] = arr[i][j - 1] = arr[i][j + 1] = 999
+            back(cost + cnt, num + 1, i, j)
+            arr[i][j] = tmp_mid
+            arr[i - 1][j] = tmp_top
+            arr[i + 1][j] = tmp_bot
+            arr[i][j - 1] = tmp_left
+            arr[i][j + 1] = tmp_right
+
+
+N = int(input())
+arr = [list(map(int, input().split())) for _ in range(N)]
+# 최대 화단비용
+answer = 3000000000
+back(0, 0, 0, 0)
 print(answer)

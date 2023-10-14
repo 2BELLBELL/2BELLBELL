8152132121285cb985d4f7ad67d6b60f787e2b06
2BELLBELL <139421092+2BELLBELL@users.noreply.github.com>
2023-10-14 21:30:12 +0900

    [Gold IV] Title: 여행 가자, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.py
+++ 백준/Gold/1976. 여행 가자/여행 가자.py
@@ -1,41 +1,44 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
 
-# 여행 갈 수 있는 지역 체크
-def bfs(v):
-    q = deque([v])
-    visited = [False] * N
-    visited[v] = True
-    while q:
-        tmp = q.popleft()
-        for i in arr[tmp]:
-            if not visited[i]:
-                visited[i] = True
-                q.append(i)
 
-    return visited
+# find 연산
+def find(target):
+    # 본인이 집합의 대표가 아니라면
+    # 집합의 대표와  같은 숫자로 변경되도록 재귀
+    if target != citys[target]:
+        citys[target] = find(citys[target])
+
+    # 집합의 대표를 반환
+    return citys[target]
+
+
+# union 연산
+def union(a, b):
+    a = find(a)
+    b = find(b)
+
+    # 작은 노드 기준으로 합친다
+    if a < b:
+        citys[b] = a
+    else:
+        citys[a] = b
+
 
 N = int(input())
 M = int(input())
-arr = [[] for _ in range(N)]
-
-# 도시 사이의 길 체크하기
+citys = list(range(N))
 for i in range(N):
     city = list(map(int, input().split()))
     for j in range(N):
         if city[j] == 1:
-            arr[i].append(j)
-plan = list(map(int, input().split()))
+            # 유니온
+            union(i, j)
 
-# 첫번째 도시로부터 갈 수 있는 모든 곳을 체크
-result = bfs(plan[0] - 1)
-
-# 가능하면 YES
-answer = 'YES'
-# 여행 계획을 순회하며 갈 수 없는 곳이 있으면 NO로 변경
-for i in plan:
-    if not result[i - 1]:
-        answer = 'NO'
-# 가능 여부 출력
-print(answer)
+travle = list(map(int, input().split()))
+result = citys[travle[0] - 1]
+for i in range(1, M):
+    if citys[travle[i] - 1] != result:
+        print('NO')
+        exit()
+print('YES')

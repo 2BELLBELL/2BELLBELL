1ef0eb26e5bdf1ab65163d53ed23f29f3dea509d
2BELLBELL <139421092+2BELLBELL@users.noreply.github.com>
2024-01-07 22:48:28 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 84 ms, Memory: 35880 KB -BaekjoonHub

--- 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.py
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.py
@@ -1,19 +1,23 @@
-# 재귀함수
-def star(N, x, y):
-    if N == 1:
-        return
-
-    for i in range(x+N//3, x+N-N//3):
-        for j in range(y+N//3, y+N-N//3):
-            arr[i][j] = ' '
-
-    for i in range(0+x, N+x, N//3):
-        for j in range(0+y, N+y, N//3):
-            star(N//3, i, j)
-
-
 N = int(input())
-arr = [['*'] * N for _ in range(N)]
-star(N, 0, 0)
-for i in arr:
-    print(''.join(i))
+
+# 재귀함수
+def star(n):
+    if n == 1:
+        return '*'
+    
+    stars = star(n//3)
+    result = []
+
+    for i in stars:
+        result.append(i * 3)
+    for i in stars:
+        result.append(i + ' ' * (n//3) + i)
+    for i in stars:
+        result.append(i * 3)
+
+    return result
+
+result = star(N)
+
+for i in result:
+    print(i)
